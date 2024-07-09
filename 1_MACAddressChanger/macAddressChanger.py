fdec2d1979adb6296a0313df08e9278f9f5295c3
Sumeet Singh <sumeet605@gmail.com>
2024-07-09 14:02:24 -0400

    fstring

--- 1_MACAddressChanger/macAddressChanger.py
+++ 1_MACAddressChanger/macAddressChanger.py
@@ -3,19 +3,21 @@
 import subprocess
 import optparse
 
+def getArgs():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change")
+    parser.add_option("-m", "--mac", dest="newMac", help="New MAC Address")
+
+    return parser.parse_args()
+
 def changeMac(interface, newMac):
-    print("[+] Changing MAC address for " + interface + " to " + newMac)
+    print(f"[+] Changing MAC address for {interface} to {newMac}")
 
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", newMac])
     subprocess.call(["ifconfig", interface, "up"])
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser()
-
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change")
-    parser.add_option("-m", "--mac", dest="newMac", help="New MAC Address")
-
-    options, arguments = parser.parse_args()
-
+    options, arguments = getArgs()
     changeMac(options.interface, options.newMac)
