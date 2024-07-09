d0280d784c14b30871fe3b10ff90310f01767055
Sumeet Singh <sumeet605@gmail.com>
2024-07-09 14:06:16 -0400

    erro

--- 1_MACAddressChanger/macAddressChanger.py
+++ 1_MACAddressChanger/macAddressChanger.py
@@ -9,12 +9,14 @@ def getArgs():
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change")
     parser.add_option("-m", "--mac", dest="newMac", help="New MAC Address")
     
+    options, arguments = parser.parse_args()
+    
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info.")
     elif not options.newMac:
         parser.error("[-] Please specify a MAC address, use --help for more info.")
 
-    return parser.parse_args()
+    return options
 
 def changeMac(interface, newMac):
     print(f"[+] Changing MAC address for {interface} to {newMac}")
@@ -24,5 +26,5 @@ def changeMac(interface, newMac):
     subprocess.call(["ifconfig", interface, "up"])
 
 if __name__ == "__main__":
-    options, arguments = getArgs()
+    options = getArgs()
     changeMac(options.interface, options.newMac)
