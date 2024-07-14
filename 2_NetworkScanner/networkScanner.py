06269f84105e5b397debf6f37f9d83d2f0cbc533
Sumeet Singh <sumeet605@gmail.com>
2024-07-14 11:06:04 -0400

    f

--- 2_NetworkScanner/networkScanner.py
+++ 2_NetworkScanner/networkScanner.py
@@ -6,9 +6,11 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arpRequestBroadcast = broadcast/arpRequest
     
-    answered = scapy.srp(arpRequestBroadcast, timeout=1)[0]
+    answered = scapy.srp(arpRequestBroadcast, timeout=1, verbose=False)[0]
+    
+    print("IP\t\t\tMAC Address\n")
+    
     for i in answered:
-        print(i[1].psrc)
-        print(i[1].hwsrc)
+        print(i[1].psrc + "\t\t" + i[1].hwsrc)
     
 scan("192.168.42.2/24")
