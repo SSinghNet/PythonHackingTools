4ce4d2617996dffc6ae18b254a053cd1ad0ffd90
Sumeet Singh <sumeet605@gmail.com>
2024-08-07 18:16:37 -0400

    asdf

---
+++ 10_Backdoor/listener.py
@@ -0,0 +1,11 @@
+import socket
+
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+listener.bind(("192.168.42.128", 4444))
+listener.listen(0)
+print("[+] Waiting for incoming connections.")
+
+listener.accept()
+print("[+] Got a connection.")
